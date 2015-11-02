e84712c89620b1a1284e71056343f3dd572b03db
Juan Pablo Marzetti <yonpols@groupon.com>
2015-11-01 23:20:28 -0800

    Implement custom switch

--- Yelp/SwitchCell.m
+++ Yelp/SwitchCell.m
@@ -7,11 +7,12 @@
 //
 
 #import "SwitchCell.h"
+#import "SevenSwitch.h"
 
 @interface SwitchCell ()
 
-@property (weak, nonatomic) IBOutlet UISwitch *valueSwitch;
-- (IBAction)valueChanged:(UISwitch *)sender;
+@property (weak, nonatomic) IBOutlet UIView *switchContainerView;
+@property (strong, nonatomic) SevenSwitch *valueSwitch;
 
 @end
 
@@ -19,10 +20,20 @@
 
 - (void)awakeFromNib {
     // Initialization code
-}
+    self.valueSwitch = [[SevenSwitch alloc] initWithFrame:CGRectMake(0, 0, 60, 31)];
+    [self.valueSwitch addTarget:self action:@selector(valueChanged) forControlEvents:UIControlEventValueChanged];
+    self.valueSwitch.thumbImage = [UIImage imageNamed:@"yelp-off"];
+    self.valueSwitch.onLabel.textColor = [UIColor whiteColor];
+    self.valueSwitch.offLabel.textColor = [UIColor whiteColor];
+    self.valueSwitch.offLabel.text = @"off";
+    self.valueSwitch.onLabel.text = @"on";
+    self.valueSwitch.inactiveColor = [UIColor colorWithRed:(189.0f/255.0f) green:(189.0f/255.0f) blue:(189.0f/255.0f) alpha:1.00f];
+    self.valueSwitch.activeColor = self.valueSwitch.inactiveColor;
+    self.valueSwitch.borderColor = self.valueSwitch.inactiveColor;
+    self.valueSwitch.onTintColor = [UIColor colorWithRed:(27.0f/255.0f) green:(134.0f/255.0f) blue:(230.0f/255.0f) alpha:1.00f];
+    self.valueSwitch.shadowColor = [UIColor whiteColor];
 
-- (IBAction)valueChanged:(UISwitch *)sender {
-    [self.delegate switchCell:self didUpdateValue:sender.on];
+    [self.switchContainerView addSubview:self.valueSwitch];
 }
 
 - (void) setOn:(BOOL)on {
@@ -32,6 +43,11 @@
 - (void) setOn:(BOOL)on animated:(BOOL)animated {
     _on = on;
     [self.valueSwitch setOn:on animated:animated];
+}
+
+- (void)valueChanged {
+    self.valueSwitch.thumbImage = [UIImage imageNamed:(self.valueSwitch.on ? @"yelp-on" : @"yelp-off")];
+    [self.delegate switchCell:self didUpdateValue:self.valueSwitch.on];
 }
 
 
